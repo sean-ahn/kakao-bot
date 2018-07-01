56ad3797237877b3ea232c1af7c667992743bae7
sean-ahn <sean9509@gmail.com>
2018-07-01 22:36:34 +0900

    define endpoints

--- handler.js
+++ handler.js
@@ -1,20 +1,5 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
-        }),
-    };
-
-    callback(null, response);
-
-    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-    // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.keyboard = (event, context, callback) => {
     const response = {
         statusCode: 200,
@@ -24,5 +9,41 @@ module.exports.keyboard = (event, context, callback) => {
         }),
     };
 
-    callback(null, response)
+    callback(null, response);
+};
+
+module.exports.message = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            type: "message",
+            buttons: ["선택 1", "선택 2", "선택 3"],
+        }),
+    };
+
+    callback(null, response);
+};
+
+module.exports.friend = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            type: "friend",
+            buttons: ["선택 1", "선택 2", "선택 3"],
+        }),
+    };
+
+    callback(null, response);
+};
+
+module.exports.chatRoom = (event, context, callback) => {
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({
+            type: "chatRoom",
+            buttons: ["선택 1", "선택 2", "선택 3"],
+        }),
+    };
+
+    callback(null, response);
 };
